0e4dc336ccb96b7b8d3571fd105f7248bee37747
Itzel Alheli Campos Berlnaga <alhelicampos1@gmail.com>
2021-05-13 11:35:47 -0500

    Añadiendo vistas

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def productos(request):
+    return render(request, 'productos.html')
+def contacto(request):
+    return render(request, 'contacto.html')
