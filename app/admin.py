f9c0f91b576c30250069fd893cc49b2ce84b7a11
Brayan Ruiz <brayanja00@hotmail.com>
2021-05-13 17:26:28 -0500

    Añadiendo modelos

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,6 @@ from app.models import Estanteria, Lockers
 from django.contrib import admin
 
 # Register your models here.
+
 admin.site.register(Estanteria)
 admin.site.register(Lockers)
--- app/models.py
+++ app/models.py
@@ -3,23 +3,23 @@ from .models import *
 
 # Create your models here.
 class Estanteria(models.Model):
-    fabricante = models.CharField(max_length=50)
-    precio = models.IntegerField()
-    color = models.CharField(max_length=50)
-    material = models.CharField(max_length=50)
-    peso = models.CharField(max_length=50)
-    dimensiones_del_producto = models.CharField(max_length=50)
-    id_est = models.IntegerField()
-    def __str__(self):
-        return self.fabricante
-        
-class Lockers(models.Model):
+    nombre = models.CharField(max_length=50)
     fabricante = models.CharField(max_length=50)
     precio = models.FloatField()
     color = models.CharField(max_length=50)
     material = models.CharField(max_length=50)
     peso = models.CharField(max_length=50)
     dimensiones_del_producto = models.CharField(max_length=50)
-    id_lock = models.IntegerField()
     def __str__(self):
-        return self.fabricante
+        return self.nombre
+        
+class Lockers(models.Model):
+    nombre = models.CharField(max_length=50)
+    fabricante = models.CharField(max_length=50)
+    precio = models.FloatField()
+    color = models.CharField(max_length=50)
+    material = models.CharField(max_length=50)
+    peso = models.CharField(max_length=50)
+    dimensiones_del_producto = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
