a6fcae662921d0a0782902b71799904df573deae
Pedro Alexander Salinas Esquivel <Pedro.Salinas.Esquivel@outlook.com>
2021-05-13 16:02:26 -0500

    Actualizacion de campos

--- app/models.py
+++ app/models.py
@@ -3,12 +3,23 @@ from .models import *
 
 # Create your models here.
 class Estanteria(models.Model):
-    descripcion = models.CharField(max_length=50)
-    precio = models.FloatField()
+    fabricante = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    color = models.CharField(max_length=50)
+    material = models.CharField(max_length=50)
+    peso = models.CharField(max_length=50)
+    dimensiones_del_producto = models.CharField(max_length=50)
+    id_est = models.IntegerField()
     def __str__(self):
-        return self.descripcion
+        return self.fabricante
         
 class Lockers(models.Model):
-    descripcion = models.CharField(max_length=60)
+    fabricante = models.CharField(max_length=50)
+    precio = models.FloatField()
+    color = models.CharField(max_length=50)
+    material = models.CharField(max_length=50)
+    peso = models.CharField(max_length=50)
+    dimensiones_del_producto = models.CharField(max_length=50)
+    id_lock = models.IntegerField()
     def __str__(self):
-        return self.nombre
+        return self.fabricante
