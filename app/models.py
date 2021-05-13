ea0e8dbec50463fe006cef2265b9517b4c3bbe11
Pedro Alexander Salinas Esquivel <Pedro.Salinas.Esquivel@outlook.com>
2021-05-13 12:49:39 -0500

    configuració de models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from .models import *
 
 # Create your models here.
+class Estanteria(models.Model):
+    descripcion = models.CharField(max_length=50)
+    precio = models.FloatField()
+    def __str__(self):
+        return self.descripcion
+        
+class Lockers(models.Model):
+    descripcion = models.CharField(max_length=60)
+    def __str__(self):
+        return self.nombre
