fbddfad998e3cf79821cddd9a2779b12a6477256
Brayan Ruiz <brayanja00@hotmail.com>
2021-05-14 00:47:26 -0500

    Finalizando modelos

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,9 @@
-from app.models import Estanteria, Lockers
+from app.models import Estanteria, Lockers, MuebleDeOficina, CajaPlastica
 from django.contrib import admin
 
 # Register your models here.
 
 admin.site.register(Estanteria)
 admin.site.register(Lockers)
+admin.site.register(MuebleDeOficina)
+admin.site.register(CajaPlastica)
--- app/models.py
+++ app/models.py
@@ -12,8 +12,30 @@ class Estanteria(models.Model):
     dimensiones_del_producto = models.CharField(max_length=50)
     def __str__(self):
         return self.nombre
-        
+
 class Lockers(models.Model):
+    nombre = models.CharField(max_length=50)
+    fabricante = models.CharField(max_length=50)
+    precio = models.FloatField()
+    color = models.CharField(max_length=50)
+    material = models.CharField(max_length=50)
+    peso = models.CharField(max_length=50)
+    dimensiones_del_producto = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+
+class MuebleDeOficina(models.Model):
+    nombre = models.CharField(max_length=50)
+    fabricante = models.CharField(max_length=50)
+    precio = models.FloatField()
+    color = models.CharField(max_length=50)
+    material = models.CharField(max_length=50)
+    peso = models.CharField(max_length=50)
+    dimensiones_del_producto = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+
+class CajaPlastica(models.Model):
     nombre = models.CharField(max_length=50)
     fabricante = models.CharField(max_length=50)
     precio = models.FloatField()
