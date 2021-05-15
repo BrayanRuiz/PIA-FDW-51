618172a5902401235754ca415842dff2b8bc2f92
Brayan Ruiz <brayanja00@hotmail.com>
2021-05-15 13:05:28 -0500

    Añadiendo las rutas y los static actualizados

--- website/urls.py
+++ website/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from app import views
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('app.urls')),
+    path('productos/', views.productos),
+    path('promociones/', views.promociones),
+    path('contacto/', views.contacto),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
